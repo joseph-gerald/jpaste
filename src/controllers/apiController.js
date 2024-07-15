648a214d6a53271801c7835a549a796d3145c85c
Joseph <contact@jooo.tech>
2024-07-15 10:08:19 +0200

    🐛 Fix paste viewer bugs

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -122,11 +122,3 @@ exports.getRobots = async (req, res) => {
 exports.getRobotsMinified = async (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'robot/robots.min.txt'));
 };
-
-exports.query = async (req, res) => {
-    console.log(req.body);
-    const query_url = `https://api.jooo.tech/encode?input=${encodeURI(req.query.query_text)}`;
-    const response = await fetch(query_url);
-    const output = await response.json();
-    res.send(output.Output);
-};
